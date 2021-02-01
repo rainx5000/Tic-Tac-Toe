a98f3aa45a5b2752db0d6d7033ee06d2e054beb1
Usman Tasayev <tasayev.usman@gmail.com>
2021-02-01 18:36:03 -0500

    Created files and worked on basic display for the TTT game, just started to work on js part of the project

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('.menu-submit-btn');
+const menu = document.querySelector('.menu');
+const menuPlayer1 = document.querySelector('#player1-input')
+
+menu.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(menuPlayer1.value)
+    console.log(menuPlayer1.value)
+})
+
+menuBtn.addEventListener('click', (e) => {
+    // console.log(menuPlayer1.)
+})
+
