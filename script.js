e0fe99f6e52007e8deca2bbfba6c225d9a66f06a
Usman Tasayev <tasayev.usman@gmail.com>
2021-02-04 18:29:29 -0500

    Been lazy with these git commits as you see, I keep forgetting too, gonna try working on it, anyway, I did a lot of bug fixes, added tie, its pretty much fully playable right now as it stands, gonna start working on styling changes

--- script.js
+++ script.js
@@ -1,209 +1,32 @@
 
-
-//create a DOM module where it will return every value of every btn on menu
-//create a controller that handles those values into the game
-//create a gameboard 
-
-
-
-// document.body.addEventListener('mousedown', (e) => {
-//     console.log(e.target.dataset.cell)
-// })
-
-// const DOM = (() => {
-//     const menuBtn = document.querySelector('.menu-submit-btn');
-//     const menuForm = document.querySelector('.menu');
-//     const Player1Input = document.querySelector('#player1-input');
-//     const Player2AIOption = document.querySelector('.player2-input-ai');
-//     const Player2HumanOption = document.querySelector('.player2-input-human');
-//     const menuPlayer2Input = document.querySelector('.player2-name');
-//     const cells = document.querySelectorAll('.cell');
-//     const turnDisplay = document.querySelector('.turn-display');
-
-//     cells.forEach((cell, index) => {
-//         cell.dataset.cell = (index+1).toString();
-//     })
-
-//     // let player1Name = '';
-//     // Player1Input.addEventListener('input', player)
-
-  
-
-//     // const player1 = function() {
-//     //     player1Name = Player1Input.value
-//     // }
-
-//     // const player1 = (Player1Input) => {
-//     //     Player1Input.addEventListener('input', player)
-//     // }
-
-
-
-    
-
-
-//     return {
-//         menuBtn,
-//         menuForm,
-//         Player1Input,
-//         Player2AIOption,
-//         Player2HumanOption,
-//         menuPlayer2Input,
-//         cells,
-//         turnDisplay,
-//         players: [Player1Input, menuPlayer2Input],
-//         // player1Name,
-//         // player1,
-//         // // player1: function() {
-//         // //     return 'hello'
-//         // // }
-//     }
-// })();
-
-
-
-
-// const menuController = (() => {
-//     const player1Input = DOM.Player1Input;
-//     const player2Input = DOM.menuPlayer2Input;
-//     const players = DOM.players;
-//     const AI = 'AI';
-
-//     let player1Name;
-//     let player2Name;
-
-
-        
-//     //event listeners update the names based on the user input
-//     player1Input.addEventListener('input', (e) => {
-//         player1Name = player1Input.value;
-//     })
-//     player2Input.addEventListener('input', (e) => {
-//         player2Name = player2Input.value;
-//     })
-   
-
-
-//     DOM.menuForm.addEventListener('click', (e) => {
-//         if (e.target.classList.contains('player2-input-ai')) {
-//             DOM.menuPlayer2Input.classList.add('display-off')
-            
-//         } else if (e.target.classList.contains('player2-input-human')) {
-//             DOM.menuPlayer2Input.classList.remove('display-off')
-//         }
-//     })
-
-//     DOM.menuForm.addEventListener('submit', (e) => {
-//         e.preventDefault();
-//         DOM.menuForm.classList.add('display-off');
-//     })
-
-//     const playerFactory = (name) => {
-//         name = name
-//         marker = name == player1Name ? 'x' : 'o'; 
-//         return {name, marker}
-//     }
-    
-
-//     return {
-//         player1: function(){
-//             return playerFactory(player1Name)
-//         },
-//         player2: function(){
-//             if (DOM.Player2AIOption.checked) {
-//                 return playerFactory(AI)
-//             } else {
-//                 return playerFactory(player2Name)
-//             }
-//         },
-//     }
-// })()
-
-
-
-
-
-
-// const gameBoard = (() => {
-// //    const player1 = DOM.player1();
-// //    const player2 = DOM.player2();
-
-//     const winConditions = [
-//         [1,2,3],
-//         [4,5,6],
-//         [7,8,9],
-//         [1,4,7],
-//         [2,5,8],
-//         [3,6,9],
-//         [1,3,9],
-//         [3,6,7]
-//     ]
-//     let current = 'x';
-    
-
-//     return {
-//         winConditions,
-//         current
-
-
-//     }
-// })()
-
-// const gameController = (() => {
-    
-// })()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // menu.addEventListener('submit', (e) => {
-    //         e.preventDefault();
-    //         player1 = playerFactory(menuPlayer1.value) //player always going to factory function
-    //         if (Player2AI.checked) {
-    //             console.log('ai')//ai to ai object
-    //         } else {
-    //             player2 = playerFactory(menuPlayer2Input.value) //player to factory function
-
-    //         }
-    //     })
-
-
-
-//when move is made, reverse the array of players which would change the current player
-
-//get our cell that we marked and put it on the board
-
-//when we click on the selected cell, we can grab the data cell and shove it into a function as an argument to use
-//to store into board
-
-
    const player = (marker) => {
-        const getMarker = () => marker
         let isWinner = false;
-
+        const getMarker = () => marker
         const makeWinner = () => gameBoard.current().isWinner = true;
+        const resetWinner = () => gameBoard.current().isWinner = false;
 
-        const rowCount = [0,0,0]; //any num to hit to 3 declares a winner;
-        const colCount = [0,0,0]; // any num to hit 3 declares a winner;
-        const diagCount = [0,0,0]; //all three nums should be 1 declares winner
-        const reverseDiagCount = [0,0,0] //all three nums should be 1 declares winner
+        let rowCount = [0,0,0]; //any num to hit to 3 declares a winner;
+        let colCount = [0,0,0]; // any num to hit 3 declares a winner;
+        let diagCount = [0,0,0]; //all three nums should be 1 declares winner
+        let reverseDiagCount = [0,0,0] //all three nums should be 1 declares winner
+
+        const resetStats = () => {
+            rowCount = [0,0,0]; 
+            colCount = [0,0,0]; 
+            diagCount = [0,0,0]; 
+            reverseDiagCount = [0,0,0];
+            isWinner = false;
+        }
+
+        const placeMarker = (cords) => {
+            gameBoard.board[cords[0]][cords[1]] = gameBoard.current().getMarker();
+            markerCount(cords[0], cords[1]);
+           }
 
         const markerCount = (row, col) => {
             colCount[col]+= 1;
             rowCount[row]+= 1;
-            console.log (`rowCount: ${rowCount} and colCount: ${colCount}`)
+            
             if (col == 1 && row == 1) {
                 diagCount[col] = 1;
                 reverseDiagCount[col] = 1;
@@ -216,33 +39,39 @@
             } else if (col == 0 && row == 2) {
                 reverseDiagCount[row] = 1;
             } 
-            checkWinner();
-            //function that would check for winner
+            console.log (`rowCount: ${rowCount} and colCount: ${colCount} diaCount: ${diagCount} revdiaG: ${reverseDiagCount}`)
+            // checkWinner();
         }
 
-        const checkWinner = () => {
-            
-            rowCount.forEach(row => {
+        const checkWinner = () => {    //will check scan the counter arrays
+            rowCount.forEach(row => {  //will check for each row having a value of 3 - if so, the player wins
                 row == '3' && makeWinner();
             })
             colCount.forEach(col => {
-                col == '3' && makeWinner();
+                col == '3' && makeWinner(); //checks for each col, having a value of 3 - if so, the player wins
             })
-            _checkEqual(diagCount) && makeWinner();
-            _checkEqual(reverseDiagCount) && makeWinner();
-            console.log(isWinner)
+            _checkEqual(diagCount) && makeWinner(); //if all the values of dia is 1 - player wins -- each value basically represents each block in the diagnol 
+            _checkEqual(reverseDiagCount) && makeWinner(); //same as above just opposite
+
+            let tie = true;
+            const board = gameBoard.board
+            for (let row in board) {
+                // tie = board[row].every(el => el.length > 0) ? true : false;     //problem is it needs to make sure ALL OF THE ARE TRUE THEN MAKE TRUE           
+                if (board[row].every(el => el.length > 0)) {
+                    tie = true;
+                } else {
+                    tie = false;
+                    break;
+                }
+            }
+
+            tie && gameBoard.makeTie()
+            // console.log(tie)
         }
 
-       const placeMarker = (cords) => {
-        gameBoard.board[cords[0]][cords[1]] = gameBoard.current().getMarker();
-        markerCount(cords[0], cords[1]);
-       }
-
-       const _checkEqual = (arr) => arr.every(num => num === 1) 
-
-
-
-
+       const _checkEqual = (arr) => arr.every(num => num === 1) //used to check if all the elements are the same value of an array used in checkWinner()
+       
+       
        return {
            getMarker,
            isWinner,
@@ -252,7 +81,10 @@
            rowCount,
            colCount,
            diagCount,
-           reverseDiagCount
+           reverseDiagCount,
+           resetStats,
+           checkWinner,
+           resetWinner,
         }
    } 
 
@@ -260,28 +92,45 @@
        const players = [player('X'), player('O')];
 
        const current = () => players[0];
-
+    //    const lastPlayer = () => players[1];
        const switchTurn = () => players.reverse();
+
        const getWinner = () => gameBoard.current().isWinner;
+       let isTie = false;
+       const makeTie = () => isTie = true;
+       const getTie = () => isTie;
+       const resetTie = () => isTie = false;
 
-       
-
-    
-
-       const board = {
+       let board = {
            0: ['','',''],
            1: ['','',''],
            2: ['','','']
        }
 
-       
+       const resetBoard = () => {
+        for (let row in board) {
+         board[row] = ['','','']
+        }   
+
+        players.forEach(player => {
+            player.resetStats();
+            console.log(player)
+        }) 
+        
+    }
+
 
        return {
            players,
            board,
            current,
            switchTurn,
-           getWinner
+           getWinner,
+           resetBoard,
+           makeTie,
+           isTie,
+           getTie,
+           resetTie
        }
 
    })()
@@ -293,34 +142,79 @@
     const player2Input = document.querySelector('#player2-input');
     const cells = document.querySelectorAll('.cell');
     const turnDisplay = document.querySelector('.turn-display');
-    const markers = document.querySelectorAll('.marker')
+    const markers = document.querySelectorAll('.marker');
+    const restartGame = document.querySelector('.game-over');
 
 
     //player names from menu to display
     menuForm.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        document.querySelector('.player1-display').textContent = player1Input.value + ' ';
-        document.querySelector('.player2-display').textContent = player2Input.value + ' ';
+        document.querySelector('.player1-display').textContent = '- ' + player1Input.value;
+        document.querySelector('.player2-display').textContent = '- ' + player2Input.value;
         
         menuForm.classList.add('display-off')
+        document.querySelector('.game-container').classList.remove('display-off', 'stopClick')
+        
+
     })
 
     cells.forEach(cell => {
        cell.addEventListener('click', (e) => {
-           if (e.target.textContent.length === 0) {
+           if (e.target.textContent.length === 0) { //lets you only click once on a spot
             e.target.textContent = gameBoard.current().getMarker();
+            gameBoard.current().placeMarker(e.target.dataset.cell.split('-')); //cords of current cell moving into gameBoard
+            gameBoard.current().checkWinner();
+            gameBoard.getWinner() 
+            console.log(gameBoard.getWinner())
+            gameBoard.current().isWinner == true && gameOver();
+            gameBoard.getTie() == true && gameOver();
+            gameBoard.switchTurn(); 
            } 
-           gameBoard.current().placeMarker(e.target.dataset.cell.split('-')); //cords of current cell moving into gameBoard
-           gameBoard.getWinner() && console.log(`GAMEOVER ${gameBoard.current().getMarker()} won`)
-           gameBoard.switchTurn();
-        
         }) 
     })
 
+
+    const gameOver = () => {
+        const markersArr = Array.from(markers);
+        const winnerDisplay = markersArr.filter(player => player.textContent == gameBoard.current().getMarker())
+        let winnerName = winnerDisplay[0].nextElementSibling.textContent;
+        gameBoard.current().isWinner && (winnerDisplay[0].nextElementSibling.textContent = winnerName + ' wins!')
+
+        gameBoard.current().resetWinner();
+        gameBoard.resetTie();
+
+        restartGame.classList.remove('display-off');
+        cells.forEach(cell => cell.classList.add('stopClick')) //stops the user from clicking any more
+    }
+
+    document.querySelector('.replay').addEventListener('click', (e) => {
+        DOMReset()
+        gameBoard.resetBoard()
+        restartGame.classList.add('display-off');
+        cells.forEach(cell => cell.classList.remove('stopClick')) //stops the user from clicking any more
+    })
+        
+    document.querySelector('.new-game').addEventListener('click', (e) => {
+        player1Input.value = '';
+        player2Input.value = '';
+        cells.forEach(cell => {cell.textContent = ''})
+        gameBoard.resetBoard()
+        restartGame.classList.add('display-off');
+        menuForm.classList.remove('display-off');
+        cells.forEach(cell => cell.classList.remove('stopClick')) //stops the user from clicking any more
+    })
+    const DOMReset = () => {
+        cells.forEach(cell => {cell.textContent = ''})
+        document.querySelector('.player1-display').textContent = '- ' + player1Input.value;
+        document.querySelector('.player2-display').textContent = '- ' + player2Input.value;
+    }
+
+
+
+    //setting unique values of each cell based on position
     let counterRow = 0;
     let counterCell = 0;
-
     cells.forEach((cell, index) => {
         cell.dataset.cell = `${counterRow}-${counterCell}`;
         counterCell++
@@ -333,9 +227,15 @@
             counterRow++
         }
     })
-    
-    
-    
-    
    })()
 
+
+
+//when its a tie - stop the game
+//ask if they want to play again or go back to menu
+
+//when all the cells are filled with a marker - its a tie 
+
+//make it show visually who's turn it is
+
+//make names required for form
